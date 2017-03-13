54c7b1e042432ce3cac64e00a1a58f040527b8ff
eric.poe11 <eric.poe11@PCP-NA-FA-20.formation.imie>
2017-03-13 11:07:06 +0100

    traduction des attributs + getter

--- pocGit/src/pocGit/DemoPoc.java
+++ pocGit/src/pocGit/DemoPoc.java
@@ -1,8 +1,12 @@
 package pocGit;
 
 public class DemoPoc {
-	private String nom;
-	private String prenom;
+	private String name;
+	private String firstname;
+	
+	public getName(){
+		return this.name;
+	}
 	//commentaire poc
 	public static void main(String[] args) {
 		System.out.println("test");
