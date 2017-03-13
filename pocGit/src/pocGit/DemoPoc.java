326f432b27de6fbbffe9705042c48de64ddda077
eric.poe11 <eric.poe11@PCP-NA-FA-20.formation.imie>
2017-03-13 11:11:55 +0100

    ajout des getters et setters

--- pocGit/src/pocGit/DemoPoc.java
+++ pocGit/src/pocGit/DemoPoc.java
@@ -7,10 +7,21 @@ public class DemoPoc {
 	public getName(){
 		return this.name;
 	}
+	
+	public getFisrtName(){
+		return this.firstname;
+	}
+	
+	public String setName(String name){
+		this.name = name;
+	}
+	
+	public String setFirstName(String firstname){
+		this.firstname = firstname;
+	}
 	//commentaire poc
 	public static void main(String[] args) {
 		System.out.println("test");
-		
 		
 		
 		
