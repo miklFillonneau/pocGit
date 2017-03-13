ec45a661e39f88c33ea533050855af6c20dcae20
michael.poe11 <michael.poe11@PCP-NA-FA-19.formation.imie>
2017-03-13 11:04:31 +0100

    ajout d'attributs

--- pocGit/src/pocGit/DemoPoc.java
+++ pocGit/src/pocGit/DemoPoc.java
@@ -1,8 +1,14 @@
 package pocGit;
 
 public class DemoPoc {
+	private String nom;
+	private String prenom;
 	//commentaire poc
 	public static void main(String[] args) {
 		System.out.println("test");
+		
+		
+		
+		
 	}
 }
